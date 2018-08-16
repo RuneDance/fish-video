46a4fb3979404fffecd03e9d1daae63a7b6f73e7
yuyongtao <smallbadoncelaugh@gmail.com>
2018-08-16 09:52:05 +0800

    modify by yyt

--- fish-video-mini-api/src/main/java/com/fishvideo/WebMvcConfig.java
+++ fish-video-mini-api/src/main/java/com/fishvideo/WebMvcConfig.java
@@ -14,7 +14,7 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/META-INF/resources/")
-                .addResourceLocations("file:C:/imooc_videos_dev/");
+                .addResourceLocations("file:C:\\imooc_videos_dev\\");
     }
 
     @Bean(initMethod = "init")
